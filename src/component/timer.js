3a7ab2dee97bcbb307e5fabc08140201ba88444c
abhishek kaushal <abhikaushalak17@gmail.com>
2024-01-01 11:44:21 +0530

    356

--- src/component/timer.js
+++ src/component/timer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 import './timer.css';
 
 const Timer = () => {
@@ -7,50 +7,51 @@ const Timer = () => {
   const [buttonClicked, setButtonClicked] = useState(false);
   const initialTimeRef = useRef(0);
 
-  useEffect(() => {
-    initialTimeRef.current = time;
-    if (!isPaused && time > 0) {
-      const timerId = setInterval(() => {
-        setTime(prevTime => prevTime - 1);
-      }, 1000);
-
-      return () => clearInterval(timerId);
-    }
-
-  }, [time, isPaused]);
-
-  const startTimer = () => {
-    setIsPaused(false);
-    setButtonClicked(true);
-  };
-
-  const stopTimer = () => {
-    setIsPaused(true);
-    setButtonClicked(false);
-  };
-
-  const resetTimer = () => {
-    setIsPaused(true);
-    initialTimeRef.current = 0; 
-    setTime(0); 
-    setButtonClicked(false);
-    document.querySelector('.input').value = 0; 
-  };
-
-  const handleInputChange = (event) => {
+  const handleInputChange = useCallback((event) => {
     const inputValue = parseInt(event.target.value, 10) || 0;
-  
+
     if (inputValue < 0) {
       alert("Value should not be less than 0");
       return;
     }
-  
+
     initialTimeRef.current = inputValue;
-  
+
     if (!buttonClicked) {
       setTime(inputValue);
     }
-  };
+  }, [buttonClicked]);
+
+  const startTimer = useCallback(() => {
+    setIsPaused(false);
+    setButtonClicked(true);
+  }, []);
+
+  const stopTimer = useCallback(() => {
+    setIsPaused(true);
+    setButtonClicked(false);
+  }, []);
+
+  const resetTimer = useCallback(() => {
+    setIsPaused(true);
+    initialTimeRef.current = 0;
+    setTime(0);
+    setButtonClicked(false);
+    document.querySelector('.input').value = "";
+  }, []);
+
+  useEffect(() => {
+    initialTimeRef.current = time;
+    if (!isPaused && time > 0) {
+      const timerId = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+
+      return () => clearInterval(timerId);
+    }
+  }, [time, isPaused]);
+
+  const formattedTime = useMemo(() => `${Math.floor(time / 60)} minute ${time % 60} sec`, [time]);
 
   return (
     <>
@@ -62,11 +63,17 @@ const Timer = () => {
           placeholder='Time ...'
           onChange={handleInputChange}
         />
-        <h1 className='timer'>Timer: {Math.floor(time / 60)}  minute {time % 60} sec</h1>
+        <h1 className='timer'>Timer: {formattedTime}</h1>
         <div>
-          <button className='start-btn' onClick={startTimer}>Start</button>
-          <button className='stop-btn' onClick={stopTimer}>Stop</button>
-          <button className='reset-btn' onClick={resetTimer}>Reset</button>
+          <button className='start-btn' onClick={startTimer}>
+            Start
+          </button>
+          <button className='stop-btn' onClick={stopTimer}>
+            Stop
+          </button>
+          <button className='reset-btn' onClick={resetTimer}>
+            Reset
+          </button>
         </div>
       </div>
     </>
